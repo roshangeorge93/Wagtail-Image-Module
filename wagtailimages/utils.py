3443dbc601323ace92c3b82e7f85eb20205bfa36
Roshan George <roshangeorge93@gmail.com>
2017-03-08 22:08:42 +0530

    Adding utility functions to create folders and images present in a physcial path, functions to convert images and folders to dict

--- wagtailimages/utils.py
+++ wagtailimages/utils.py
@@ -1,5 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+
+from wagtail.wagtailsearch import index as search_index
+from wagtail.wagtailimages.models import get_folder_model
+from wagtail.wagtailimages.forms import get_image_form
+from wagtail.wagtailimages.fields import ALLOWED_EXTENSIONS
+from wagtail.wagtailimages import get_image_model
+
+from django.core.files import File
+
+ImageFolder = get_folder_model()
+Image = get_image_model()
+ImageForm = get_image_form(Image)
+
 
 # Helper functions for migrating the Rendition.filter foreign key to the filter_spec field,
 # and the corresponding reverse migration
@@ -39,3 +53,96 @@ def get_fill_filter_spec_migrations(app_name, rendition_model_name):
                 Rendition.objects.using(db_alias).filter(filter_spec=filter_spec).update(filter=filter)
 
     return (fill_filter_spec_forward, fill_filter_spec_reverse)
+
+
+def create_db_entries(title, user, parent_folder=None):
+    """Recursively creates DB entries for a physical folder, its sub folders
+    and images under them."""
+
+    folder = ImageFolder()
+    folder.title = title
+    if parent_folder:
+        folder.folder = parent_folder
+
+    try:
+        folder.save()
+    except FileExistsError:
+        # Ignore the exception as the physical folder already exists
+        pass
+
+    complete_path = folder.get_complete_path()
+
+    for entry in os.listdir(complete_path):
+        # Get all the files and folders under the current folder
+        # If a file is found and if its of an image type, create an image entry
+        if os.path.isfile(os.path.join(complete_path, entry)):
+            extension = os.path.splitext(entry)[1]  # get the extension
+            extension = extension[1:]   # Remove the '.' from the extension
+            if extension in ALLOWED_EXTENSIONS:
+                create_image(entry, user, folder)
+
+        # Recursively do the same for the sub folders
+        if os.path.isdir(os.path.join(complete_path, entry)):
+            create_db_entries(entry, user, folder)
+
+    return folder
+
+
+def create_image(file_name, user, folder):
+    """Creates an image object."""
+
+    image_file = File(open(os.path.join(folder.get_complete_path(), file_name), 'rb'))
+
+    # Build a form for validation
+    form = ImageForm({
+        'title': os.path.splitext(file_name)[0],
+        'collection': '1',  # Hard coding the root collection as default
+    }, {
+        'file': image_file
+    }, user=user)
+
+    if form.is_valid():
+        # Save it
+        image = form.save(commit=False)
+        image.uploaded_by_user = user
+        image.file_size = image.file.size
+        image.folder = folder
+        image.save()
+
+        # Reindex the image to make sure all tags are indexed
+        search_index.insert_or_update_object(image)
+
+        return image
+    return None
+
+
+def get_folders_list(folders):
+    """Recursively converts a list of folder objects to a list of folder dicts.
+    Also returns a list of images under each folder in a dictionary format."""
+
+    folders_list = list()
+    for folder in folders:
+        folder_dict = dict()
+        folder_dict['id'] = folder.id
+        folder_dict['title'] = folder.title
+
+        # Add images
+        folder_dict['images'] = list()
+        images = Image.objects.filter(folder=folder)
+        for image in images:
+            folder_dict['images'].append(get_image_dict(image))
+
+        # Get the contents of the sub folder
+        folder_dict['sub_folders'] = get_folders_list(folder.get_subfolders())
+        folders_list.append(folder_dict)
+    return folders_list
+
+
+def get_image_dict(image):
+    """Converts an image object to dictionary containing the core fields of an image."""
+
+    image_dict = dict()
+    image_dict['id'] = image.id
+    image_dict['title'] = image.title
+    image_dict['url'] = image.file.url
+    return image_dict
