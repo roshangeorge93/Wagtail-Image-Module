7b013b7a83202b30d1fe56c69934514e8f2c9943
Roshan George <roshangeorge93@gmail.com>
2017-03-09 01:35:37 +0530

    Hnadled edge cases for adding and editing folders

--- wagtailimages/api/custom/folders.py
+++ wagtailimages/api/custom/folders.py
@@ -5,7 +5,11 @@ from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 from wagtail.wagtailimages.models import get_folder_model
 from wagtail.wagtailimages.permissions import permission_policy
-from wagtail.wagtailimages.utils import get_folders_list, create_db_entries
+from wagtail.wagtailimages.utils import (
+    get_folders_list,
+    create_db_entries,
+    get_image_dict
+)
 from wagtail.wagtailimages import get_image_model
 from wagtail.wagtailsearch import index as search_index
 
@@ -14,6 +18,9 @@ Image = get_image_model()
 
 
 def list(request, folder_id=None):
+    """Returns a list of folders and images under them in JSON compatible format.
+    Optional folder id can be passed to get folders and images under a specific folder.
+    """
 
     if not permission_policy.user_has_any_permission(request.user, ['add', 'change', 'delete']):
         response = {
@@ -23,59 +30,25 @@ def list(request, folder_id=None):
 
     if folder_id:
         folders = ImageFolder.objects.filter(id=folder_id)
+        folders_list = get_folders_list(folders)
     else:
+        root_folder = dict()
+        root_folder['id'] = '-1'
+        root_folder['title'] = 'root'
+
+        # Get all folders under root
         folders = ImageFolder.objects.filter(folder__isnull=True)
+        root_folder['sub_folders'] = get_folders_list(folders)
 
-    folders_list = get_folders_list(folders)
+        # Get all images under root
+        images = Image.objects.filter(folder__isnull=True)
+        root_folder['images'] = list()
+        for image in images:
+            root_folder['images'].append(get_image_dict(image))
+
+        folders_list = [root_folder]
+
     return JsonResponse(folders_list, safe=False)
-
-
-@require_POST
-def add(request, parent_id=None):
-    response = dict()
-
-    if not permission_policy.user_has_permission(request.user, 'add'):
-        response['message'] = "User does not have permission"
-        return JsonResponse(response, status=403)
-
-    title = request.POST.get('title')
-
-    if not title:
-        response['message'] = "Title missing"
-        return JsonResponse(response, status=400)
-
-    parent_folder = None
-    if parent_id:
-        try:
-            parent_folder = ImageFolder.objects.get(id=parent_id)
-        except ObjectDoesNotExist:
-            response['message'] = "Invalid Parent ID"
-            return JsonResponse(response, status=404)
-
-    if parent_folder:
-        if ImageFolder.objects.filter(folder=parent_folder, title=title).count() > 0:
-            response['message'] = "Folder already exists"
-            return JsonResponse(response, status=403)
-    else:
-        if ImageFolder.objects.filter(folder__isnull=True, title=title).count() > 0:
-            response['message'] = "Folder already exists"
-            return JsonResponse(response, status=403)
-
-    # Save folder
-    folder = ImageFolder(
-        title=title
-    )
-    if parent_folder:
-        folder.folder = parent_folder
-    folder.save()
-
-    folder_dict = dict()
-    folder_dict['id'] = folder.id
-    folder_dict['title'] = folder.title
-
-    response['message'] = "Success"
-    response['data'] = folder_dict
-    return JsonResponse(response)
 
 
 @require_POST
@@ -119,7 +92,7 @@ def move(request):
         extension = os.path.splitext(image.filename)[1]     # get the extension
         new_path = os.path.join(target_folder.path, complete_file_name)
         while True:
-            # Keeping renaming in a loop to handle multiple filename clashes
+            # Keeping renaming until there are no more filename clashes
             if os.path.exists(new_path):
                 count += 1
                 complete_file_name = file_name + str(count) + extension
@@ -127,7 +100,11 @@ def move(request):
             else:
                 os.rename(initial_path, new_path)
                 if count:
+                    # If there were attempts made to resolve conflict
                     image.title = image.title + str(count)
+                    response['message'] = "File with the same name exists! Renaming to avoid conflict."
+                else:
+                    response['message'] = "Success"
                 break
 
         image.file.name = os.path.join(target_folder.path, complete_file_name)
@@ -158,19 +135,73 @@ def move(request):
                     new_folder = create_db_entries(source_folder.title, request.user, target_folder)
                     folders_list = get_folders_list([new_folder])
                     response['message'] = "Operation Failed! Found new entry in the OS. Loading the folder..."
-                    response['new_folders'] = folders_list
+                    response['new_folder'] = folders_list[0]
                     # Return a 202 as the intended action was not completed
                     return JsonResponse(response, status=202)
             else:
+                if count:
+                    # If there were attempts made to resolve conflict
+                    response['message'] = "Folder with the same name exists! Renaming to avoid conflict."
+                else:
+                    response['message'] = "Success"
                 break
         source_folder.save()
         response['new_source_name'] = source_folder.title
-
+        return JsonResponse(response)
     else:
         response['message'] = "Invalid source type"
         return JsonResponse(response, status=400)
 
-    response['message'] = "Success"
+
+
+@require_POST
+def add(request, parent_id=None):
+    response = dict()
+
+    if not permission_policy.user_has_permission(request.user, 'add'):
+        response['message'] = "User does not have permission"
+        return JsonResponse(response, status=403)
+
+    title = request.POST.get('title')
+
+    if not title:
+        response['message'] = "Title not passed"
+        return JsonResponse(response, status=400)
+
+    parent_folder = None
+    if parent_id:
+        try:
+            parent_folder = ImageFolder.objects.get(id=parent_id)
+        except ObjectDoesNotExist:
+            response['message'] = "Invalid Parent ID"
+            return JsonResponse(response, status=404)
+
+    # Save folder
+    folder = ImageFolder(
+        title=title
+    )
+    if parent_folder:
+        folder.folder = parent_folder
+    try:
+        # Check if the folder is present in the DB or physically present in the OS
+        folder.validate_folder()
+    except ValidationError as e:
+        if e.code == 'os':
+            # If present in the OS, then create the folder entry and it's
+            # content's entries in the DB
+            folder = create_db_entries(title, request.user, parent_folder)
+            folders_list = get_folders_list([folder])
+            response['message'] = "Found new entry in the OS! Loading the folder..."
+            response['data'] = folders_list[0]
+        else:
+            response['message'] = "Folder already exists"
+            return JsonResponse(response, status=403)
+    else:
+        # Save folder
+        folder.save()
+        response['message'] = "Success"
+        response['data'] = get_folders_list([folder])[0]
+
     return JsonResponse(response)
 
 
@@ -187,7 +218,6 @@ def edit(request, folder_id):
     except ObjectDoesNotExist:
         response['message'] = "Invalid ID"
         return JsonResponse(response, status=404)
-    title = request.POST.get('title', folder.title)
 
     parent_folder = None
     parent_id = request.POST.get('parent_id')
@@ -198,22 +228,38 @@ def edit(request, folder_id):
             response['message'] = "Invalid Parent ID"
             return JsonResponse(response, status=404)
 
-    if parent_folder:
-        if ImageFolder.objects.filter(folder=parent_folder, title=title).count() > 0:
-            response['message'] = "Folder already exists"
-            return JsonResponse(response, status=403)
-    else:
-        if ImageFolder.objects.filter(folder__isnull=True, title=title).count() > 0:
-            response['message'] = "Folder already exists"
-            return JsonResponse(response, status=403)
-
+    title = request.POST.get('title', folder.title)
     folder.title = title
     if parent_folder:
         folder.folder = parent_folder
-    folder.folder = request.POST.get('parent_id', folder.folder)
-    folder.save()
+    count = 0   # Indicates the number of times the folder name clashed
+    while True:
+        try:
+            # Check if the folder is present in the DB or physically present in the OS
+            folder.validate_folder()
+        except ValidationError as e:
+            count += 1
+            if e.code == 'db':
+                folder.title = folder.title + str(count)
+            else:
+                # When a folder with a clashing name exists in the OS,
+                # Add the entry to the DB and notify the user.
+                # Abort the current move operation
+                new_folder = create_db_entries(title, request.user, parent_folder)
+                folders_list = get_folders_list([new_folder])
+                response['message'] = "Operation Failed! Found new entry in the OS. Loading the folder..."
+                response['data'] = folders_list[0]
+                # Return a 202 as the intended action was not completed
+                return JsonResponse(response, status=202)
+        else:
+            if count:
+                # If there were attempts made to resolve conflict
+                response['message'] = "Folder with the same name exists! Renaming to avoid conflict."
+            else:
+                response['message'] = "Success"
+            break
 
-    response['message'] = "Success"
+    response['data'] = get_folders_list([folder])[0]
     return JsonResponse(response)
 
 
